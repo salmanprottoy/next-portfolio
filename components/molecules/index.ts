5ec6d3a7c7e79c8aa3d972fb47851e82012dba74
salmanprottoy <salman.prottoy@gmail.com>
2025-07-08 01:55:22 +0600

    # Improvement - performance optimization

--- components/molecules/index.ts
+++ components/molecules/index.ts
@@ -1,6 +1,4 @@
 export { default as TypingEffect } from "./TypingEffect";
 export { default as ProjectCard } from "./ProjectCard";
--export * from "./WorkExperienceCard";
-+export { default as WorkExperienceCard } from "./WorkExperienceCard";
-+// If you also need to expose the props interface:
-+export type { WorkExperienceCardProps } from "./WorkExperienceCard";
+export { default as WorkExperienceCard } from "./WorkExperienceCard";
+export type { WorkExperienceCardProps } from "./WorkExperienceCard";
