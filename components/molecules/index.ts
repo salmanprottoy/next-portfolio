7d02b88ff5122abde248a408ea73f6cf73d40f4a
Md. Salman Hossan Prottoy <salman.prottoy@gmail.com>
2025-07-08 01:43:17 +0600

    Update components/molecules/index.ts
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- components/molecules/index.ts
+++ components/molecules/index.ts
@@ -1,3 +1,6 @@
 export { default as TypingEffect } from "./TypingEffect";
 export { default as ProjectCard } from "./ProjectCard";
-export * from "./WorkExperienceCard";
+-export * from "./WorkExperienceCard";
++export { default as WorkExperienceCard } from "./WorkExperienceCard";
++// If you also need to expose the props interface:
++export type { WorkExperienceCardProps } from "./WorkExperienceCard";
